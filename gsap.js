50deaced06951a28fe395620855527373058ed6a
Jason Nutt <jnutt367@gmail.com>
2021-04-17 16:22:05 -0400

    commit up to h2 rotate down animation working on cousework

--- gsap.js
+++ gsap.js
@@ -1,3 +1,4 @@
 var tl = gsap.timeline()
     .to("body", { duration: 3, backgroundColor: "blue" })
-    .to("h1", { text: "typewriter effect with GSAP3", duration: 3, color: "white" })
+    .to("h1", { scale: "1.1", x: "50", text: "Typewriter effect with <br> GSAP3...BOOM did it!", ease: "power1.in", duration: 3, color: "white" })
+    .to("h2", { y: 300, rotate: 360, fontFamily: "cursive", color: "white" })
